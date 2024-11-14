de04b6bfb24bf3920dbf51b8b888733fcfc88e61
Skrycer <ciceroserre@gmail.com>
2024-11-14 18:09:53 +0000

    Creating and Using a State Class

---
+++ Assets/GameEnviroment.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public sealed class GameEnvironment : MonoBehaviour
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(
+                    GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
